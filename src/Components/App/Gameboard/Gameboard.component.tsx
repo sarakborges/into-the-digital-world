a90bd45251e72369faae5cdae4800345a5d597eb
Sara Borges <sarakborges@outlook.com>
2026-04-17 04:39:38 -0300

    split into components

---
+++ src/Components/App/Gameboard/Gameboard.component.tsx
@@ -0,0 +1,41 @@
+import { useGame } from '@/Hooks/Game.hook'
+
+import * as Zones from '@/GameData/Zones'
+
+import { Portrait } from '@/Components/System/Portrait'
+
+import './Gameboard.style.scss'
+
+export const Gameboard = () => {
+  const { game } = useGame()
+
+  if (!game) {
+    return <></>
+  }
+
+  const currentMap = { ...Zones[game.currentMap] }
+
+  return (
+    <main className="gameboard">
+      {Object.keys(currentMap.grid).map((row) => (
+        <div className="gameboard-row" key={`map-row-${row}`}>
+          {Object.keys(currentMap.grid[row]).map((col) => (
+            <div className="gameboard-col" key={`map-row-${row}-col-${col}`}>
+              <div className="tile">
+                {game.currentX === Number(col) &&
+                  game.currentY === Number(row) && (
+                    <div className="player-character">
+                      <Portrait
+                        src="/digimon_portraits/AGUMON.jpg"
+                        alt="Player character"
+                      />
+                    </div>
+                  )}
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </main>
+  )
+}
--- src/Components/Templates/Gameboard/Gameboard.template.tsx
+++ src/Components/Templates/Gameboard/Gameboard.template.tsx
@@ -1,10 +1,7 @@
 import { useGame } from '@/Hooks/Game.hook'
 
-import * as Zones from '@/GameData/Zones'
-
-import { Portrait } from '@/Components/System/Portrait'
-
 import { Controller } from '@/Components/App/Controller'
+import { Gameboard } from '@/Components/App/Gameboard'
 
 import './Gameboard.style.scss'
 
@@ -15,36 +12,10 @@ export const GameboardTemplate = () => {
     return <></>
   }
 
-  const currentMap = { ...Zones[game.currentMap] }
-
   return (
     <div className="game-body">
       <div className="main-game">
-        <main className="gameboard">
-          {Object.keys(currentMap.grid).map((row) => (
-            <div className="gameboard-row" key={`map-row-${row}`}>
-              {Object.keys(currentMap.grid[row]).map((col) => (
-                <div
-                  className="gameboard-col"
-                  key={`map-row-${row}-col-${col}`}
-                >
-                  <div className="tile">
-                    {game.currentX === Number(col) &&
-                      game.currentY === Number(row) && (
-                        <div className="player-character">
-                          <Portrait
-                            src="/digimon_portraits/AGUMON.jpg"
-                            alt="Player character"
-                          />
-                        </div>
-                      )}
-                  </div>
-                </div>
-              ))}
-            </div>
-          ))}
-        </main>
-
+        <Gameboard />
         <Controller />
       </div>
     </div>
