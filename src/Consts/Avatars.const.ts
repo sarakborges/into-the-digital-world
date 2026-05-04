14360286107a756cce54a0f0ed7fe689ba97aea2
Sara Borges <sarakborges@outlook.com>
2026-05-04 14:25:09 -0300

    added 3 colors of clothes

--- src/Consts/Avatars.const.ts
+++ src/Consts/Avatars.const.ts
@@ -8,12 +8,15 @@ export const AVATAR_OPTIONS = {
   eyes: ['BLUE', 'BROWN', 'GREEN', 'PURPLE', 'DARK_BROWN', 'LIGHT_BROWN'],
 
   clothes: [
+    '1-BLACK',
+    '1-GRAY',
+    '1-WHITE',
     '1-BLUE',
     '1-RED',
-    '1-PINK',
     '1-GREEN',
+    '1-YELLOW',
+    '1-PINK',
     '1-ORANGE',
-    '1-PURPLE',
-    '1-YELLOW'
+    '1-PURPLE'
   ]
 }
