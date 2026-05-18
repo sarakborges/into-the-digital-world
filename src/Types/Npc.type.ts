371eaa77f80bf192e633c8da77609f98573e5754
Sara Borges <sarakborges@outlook.com>
2026-05-18 17:44:07 -0300

    fixed type

--- src/Types/Npc.type.ts
+++ src/Types/Npc.type.ts
@@ -2,7 +2,7 @@ export type NpcType = {
   id: string
   title?: string
   name: string
-  description: string
+  description?: string
   portrait: string
   category?: 'appmon' | 'digimon' | 'general'
 }
