4128583615cec5339c7a0ba746150e7b1527c2e2
Sara Borges <sarakborges@outlook.com>
2026-06-21 23:31:17 -0300

    new customization options

--- src/Consts/Avatars.const.ts
+++ src/Consts/Avatars.const.ts
@@ -3,11 +3,28 @@ export const AVATAR_OPTIONS = {
   skin: ['1', '2', '3', '4', '5', '6'],
   hair: ['1', '2', '3', '4', '5', '6', '7', '8'],
 
-  hairColor: ['blonde', 'black'],
+  hairColor: ['blonde', 'black', 'brunette', 'ginger', 'albino'],
 
-  eyes: ['teal', 'amber', 'green', 'lilac', 'yellow', 'brown'],
+  eyes: [
+    'teal',
+    'amber',
+    'green',
+    'lilac',
+    'yellow',
+    'brown',
+    'orange',
+    'grey',
+    'red',
+    'purple',
+    'pink',
+    'indigo',
+    'blue'
+  ],
 
   clothes: [
+    '1-black',
+    '1-grey',
+    '1-white',
     '1-blue',
     '1-red',
     '1-green',
