b02cb0ecf5baf420c6c3be290197dab8b2a126ef
Sara Borges <sarakborges@outlook.com>
2026-07-07 23:14:47 -0300

    changed how translations work

---
+++ src/Helpers/Language/getTranslation.helper.ts
@@ -0,0 +1,34 @@
+import { loadData } from '@/Helpers/Systems/Data'
+
+import * as EnDialogs from '@/GameData/Dialogs/En'
+import * as EnTexts from '@/GameData/Texts/En'
+
+export const getTranslation = (
+  key: string,
+  replace?: Record<string, string>
+): string => {
+  const defaultLanguage = 'en'
+
+  const translations = {
+    en: { ...EnDialogs, ...EnTexts }
+  }
+
+  const settings = loadData({ key: 'settings' })
+  const lang = settings?.language || defaultLanguage
+
+  let value = translations[lang][key]
+
+  if (!value) {
+    return ''
+  }
+
+  if (!replace) {
+    return value
+  }
+
+  for (const replaceKey of Object.keys(replace)) {
+    value = value.replaceAll(replaceKey, replace[replaceKey])
+  }
+
+  return value
+}
