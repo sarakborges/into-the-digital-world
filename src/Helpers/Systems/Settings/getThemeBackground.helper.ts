693d71c5b2a72e5404863ba590acd886b0a563c8
Sara Borges <sarakborges@outlook.com>
2026-07-24 20:13:15 -0300

    validate browser persistence boundaries

--- src/Helpers/Systems/Settings/getThemeBackground.helper.ts
+++ src/Helpers/Systems/Settings/getThemeBackground.helper.ts
@@ -1,15 +1,10 @@
 import type { ThemeId } from '@/Consts/Themes.const'
-import {
-  hasThemeBackground,
-  isThemeId
-} from '@/Consts/Themes.const'
+import { hasThemeBackground } from '@/Consts/Themes.const'
 
 import { useSettingsStore } from '@/Stores/Settings.store'
 
 export const getThemeBackground = (): ThemeId => {
-  const theme = useSettingsStore.getState().settings?.theme
+  const theme = useSettingsStore.getState().settings.theme
 
-  return theme && isThemeId(theme) && hasThemeBackground(theme)
-    ? theme
-    : 'default'
+  return hasThemeBackground(theme) ? theme : 'default'
 }
