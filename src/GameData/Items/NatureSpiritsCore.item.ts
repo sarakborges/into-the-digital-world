e8c3d0702bdb7a06e56a74effaf7980d5c4d286d
Sara Borges <sarakborges@outlook.com>
2026-07-23 23:31:36 -0300

    preserve nature spirits core item id literal

--- src/GameData/Items/NatureSpiritsCore.item.ts
+++ src/GameData/Items/NatureSpiritsCore.item.ts
@@ -1,8 +1,8 @@
 import type { ItemType } from '@/Types/Item.type'
 
-export const NatureSpiritsCoreItem: ItemType = {
+export const NatureSpiritsCoreItem = {
   id: 'natureSpiritsCore',
   name: `NatureSpirits Core`,
   portrait: 'families/nsp',
   category: 'core'
-}
+} satisfies ItemType
