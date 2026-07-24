570cdf2da7ce44ec62cf4db3d4aab6d24bc5f12b
Sara Borges <sarakborges@outlook.com>
2026-07-23 23:32:28 -0300

    type research item requirements by item id

--- src/Types/Research.type.ts
+++ src/Types/Research.type.ts
@@ -1,13 +1,9 @@
+import type { ItemId } from '@/GameData/Registries/Item.registry'
+
+type ResearchItems = Partial<Record<ItemId, number>>
+
 export type ResearchType = {
-  cost: {
-    [itemId: string]: number
-  }
-
-  requiredItems?: {
-    [itemId: string]: number
-  }
-
-  optionalItems?: {
-    [itemId: string]: number
-  }
+  cost: ResearchItems
+  requiredItems?: ResearchItems
+  optionalItems?: ResearchItems
 }
