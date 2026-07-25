e0202ba30548836b100e62582d363f4d49f20b87
Sara Borges <sarakborges@outlook.com>
2026-07-25 18:18:17 -0300

    use new avatar model in customization store

--- src/Stores/AvatarCustomization.store.ts
+++ src/Stores/AvatarCustomization.store.ts
@@ -1,31 +1,28 @@
 import { create } from 'zustand'
 
-import type { AvatarType } from '@/Types/Avatar.type'
+import type {
+  AvatarCustomizationLayer,
+  AvatarType
+} from '@/Types/Avatar.type'
+
+import { DEFAULT_AVATAR } from '@/Consts/Avatars.const'
+
+type AvatarCustomizationState = {
+  avatar: AvatarType
+  layer?: AvatarCustomizationLayer
+}
 
 type AvatarCustomizationStore = {
-  avatarCustomization: {
-    avatar: AvatarType
-    layer?: keyof AvatarType
-  } | null
+  avatarCustomization: AvatarCustomizationState | null
   setAvatarCustomization: (
-    avatarCustomization: {
-      avatar: AvatarType
-      layer?: keyof AvatarType
-    } | null
+    avatarCustomization: AvatarCustomizationState | null
   ) => void
 }
 
 export const useAvatarCustomizationStore = create<AvatarCustomizationStore>(
   (set) => ({
     avatarCustomization: {
-      avatar: {
-        expression: '',
-        skin: '',
-        eyes: '',
-        hair: '',
-        hairColor: '',
-        clothes: ''
-      }
+      avatar: DEFAULT_AVATAR
     },
     setAvatarCustomization: (avatarCustomization) => {
       set({ avatarCustomization })
