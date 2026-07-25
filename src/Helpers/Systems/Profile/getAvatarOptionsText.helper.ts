3c53aa8ef297d54e89814bcc6c8df0634c802a34
Sara Borges <sarakborges@outlook.com>
2026-07-25 18:18:59 -0300

    label new avatar customization layers

--- src/Helpers/Systems/Profile/getAvatarOptionsText.helper.ts
+++ src/Helpers/Systems/Profile/getAvatarOptionsText.helper.ts
@@ -1,11 +1,15 @@
+import type { AvatarCustomizationLayer } from '@/Types/Avatar.type'
+
 import { getTexts } from '@/Helpers/Language/getTexts.helper'
 
-export const getAvatarOptionsText = (): { [layer: string]: string } => {
-  return {
-    skin: getTexts('AVATARCUSTOMIZATION_SKIN'),
-    hair: getTexts('AVATARCUSTOMIZATION_HAIR'),
-    hairColor: getTexts('AVATARCUSTOMIZATION_HAIR_COLOR'),
-    eyes: getTexts('AVATARCUSTOMIZATION_EYES'),
-    clothes: getTexts('AVATARCUSTOMIZATION_CLOTHES')
-  }
-}
+export const getAvatarOptionsText = (): Record<
+  AvatarCustomizationLayer,
+  string
+> => ({
+  skinColor: getTexts('AVATARCUSTOMIZATION_SKIN'),
+  eyeColor: getTexts('AVATARCUSTOMIZATION_EYES'),
+  hairModel: getTexts('AVATARCUSTOMIZATION_HAIR'),
+  hairColor: getTexts('AVATARCUSTOMIZATION_HAIR_COLOR'),
+  fullClothesModel: getTexts('AVATARCUSTOMIZATION_CLOTHES'),
+  fullClothesColor: getTexts('AVATARCUSTOMIZATION_CLOTHES')
+})
