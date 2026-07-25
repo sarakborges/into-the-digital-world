439147c51e867a8a7ab3e137bd4742dc9f90613f
Sara Borges <sarakborges@outlook.com>
2026-07-25 18:19:47 -0300

    adapt avatar option editing to nested model

--- src/Components/Digivice/Apps/AppAvatarCustomization/Options/AvatarCustomizationOptions.component.tsx
+++ src/Components/Digivice/Apps/AppAvatarCustomization/Options/AvatarCustomizationOptions.component.tsx
@@ -1,6 +1,20 @@
+import type {
+  AvatarCustomizationLayer,
+  AvatarType,
+  HexColor
+} from '@/Types/Avatar.type'
+
 import { getAvatarOptionsText } from '@/Helpers/Systems/Profile/getAvatarOptionsText.helper'
 
-import { AVATAR_OPTIONS } from '@/Consts/Avatars.const'
+import {
+  EYE_OPTIONS,
+  FULL_CLOTHES_MODELS,
+  HAIR_COLOR_OPTIONS,
+  HAIR_MODELS,
+  SKIN_OPTIONS,
+  getFullClothesColorOptions,
+  getFullClothesDefaultColor
+} from '@/Consts/Avatars.const'
 
 import { useAvatarCustomizationStore } from '@/Stores/AvatarCustomization.store'
 
@@ -8,6 +22,111 @@ import { Button } from '@/Components/DesignSystem/Button/Button.component'
 import { Text } from '@/Components/DesignSystem/Text/Text.component'
 import '@/Components/Digivice/Apps/AppAvatarCustomization/Options/AvatarCustomizationOptions.style.scss'
 import { PlayerAvatar } from '@/Components/Global/PlayerAvatar/PlayerAvatar.component'
+
+const getLayerOptions = (
+  layer: AvatarCustomizationLayer,
+  avatar: AvatarType
+): readonly string[] => {
+  switch (layer) {
+    case 'skinColor':
+      return SKIN_OPTIONS.map(({ color }) => color)
+    case 'eyeColor':
+      return EYE_OPTIONS.map(({ color }) => color)
+    case 'hairModel':
+      return HAIR_MODELS
+    case 'hairColor':
+      return HAIR_COLOR_OPTIONS.map(({ color }) => color)
+    case 'fullClothesModel':
+      return FULL_CLOTHES_MODELS
+    case 'fullClothesColor':
+      return getFullClothesColorOptions(
+        avatar.clothes.fullClothes?.model ?? FULL_CLOTHES_MODELS[0]
+      ).map(({ color }) => color)
+  }
+}
+
+const getLayerValue = (
+  layer: AvatarCustomizationLayer,
+  avatar: AvatarType
+): string => {
+  switch (layer) {
+    case 'skinColor':
+      return avatar.skinColor
+    case 'eyeColor':
+      return avatar.eyeColor
+    case 'hairModel':
+      return avatar.hair.model
+    case 'hairColor':
+      return avatar.hair.color
+    case 'fullClothesModel':
+      return avatar.clothes.fullClothes?.model ?? ''
+    case 'fullClothesColor':
+      return avatar.clothes.fullClothes?.color ?? ''
+  }
+}
+
+const updateAvatarLayer = (
+  avatar: AvatarType,
+  layer: AvatarCustomizationLayer,
+  option: string
+): AvatarType => {
+  switch (layer) {
+    case 'skinColor':
+      return {
+        ...avatar,
+        skinColor: option as HexColor
+      }
+    case 'eyeColor':
+      return {
+        ...avatar,
+        eyeColor: option as HexColor
+      }
+    case 'hairModel':
+      return {
+        ...avatar,
+        hair: {
+          ...avatar.hair,
+          model: option
+        }
+      }
+    case 'hairColor':
+      return {
+        ...avatar,
+        hair: {
+          ...avatar.hair,
+          color: option as HexColor
+        }
+      }
+    case 'fullClothesModel':
+      return {
+        ...avatar,
+        clothes: {
+          ...avatar.clothes,
+          mode: 'fullClothes',
+          fullClothes: {
+            model: option,
+            color: getFullClothesDefaultColor(option)
+          }
+        }
+      }
+    case 'fullClothesColor': {
+      const model =
+        avatar.clothes.fullClothes?.model ?? FULL_CLOTHES_MODELS[0]
+
+      return {
+        ...avatar,
+        clothes: {
+          ...avatar.clothes,
+          mode: 'fullClothes',
+          fullClothes: {
+            model,
+            color: option as HexColor
+          }
+        }
+      }
+    }
+  }
+}
 
 export const AvatarCustomizationOptions = () => {
   const { avatarCustomization, setAvatarCustomization } =
@@ -17,48 +136,44 @@ export const AvatarCustomizationOptions = () => {
     return
   }
 
-  const options = getAvatarOptionsText()
+  const layer = avatarCustomization.layer
+  const labels = getAvatarOptionsText()
+  const options = getLayerOptions(layer, avatarCustomization.avatar)
+  const selectedOption = getLayerValue(layer, avatarCustomization.avatar)
 
-  const updateCustomization = (option) => {
-    const avatar = {
-      ...avatarCustomization.avatar,
-      [avatarCustomization.layer!]: option
-    }
-
+  const updateCustomization = (option: string) => {
     setAvatarCustomization({
       ...avatarCustomization,
-      avatar
+      avatar: updateAvatarLayer(avatarCustomization.avatar, layer, option)
     })
   }
 
   return (
     <div className="avatar-customization-options">
       <header>
-        <Text>{options[avatarCustomization.layer]}:</Text>
+        <Text>{labels[layer]}:</Text>
       </header>
 
       <main className="customization-options">
         <div>
-          {AVATAR_OPTIONS[avatarCustomization.layer].map((option) => (
-            <div
-              key={`customization-layer-${avatarCustomization.layer}-${option}`}
-            >
-              <Button
-                disabled={
-                  avatarCustomization.avatar[avatarCustomization.layer!] ===
-                  option
-                }
-                onClick={() => updateCustomization(option)}
-              >
-                <PlayerAvatar
-                  replaceAvatar={{
-                    ...avatarCustomization.avatar,
-                    [avatarCustomization.layer!]: option
-                  }}
-                />
-              </Button>
-            </div>
-          ))}
+          {options.map((option) => {
+            const previewAvatar = updateAvatarLayer(
+              avatarCustomization.avatar,
+              layer,
+              option
+            )
+
+            return (
+              <div key={`customization-layer-${layer}-${option}`}>
+                <Button
+                  disabled={selectedOption === option}
+                  onClick={() => updateCustomization(option)}
+                >
+                  <PlayerAvatar replaceAvatar={previewAvatar} />
+                </Button>
+              </div>
+            )
+          })}
         </div>
       </main>
     </div>
