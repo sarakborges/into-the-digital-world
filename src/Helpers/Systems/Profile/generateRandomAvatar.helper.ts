cb1cfe065f646024027fb3fdb316b2e4b35dc9fe
Sara Borges <sarakborges@outlook.com>
2026-07-25 18:18:31 -0300

    generate avatars with new appearance model

--- src/Helpers/Systems/Profile/generateRandomAvatar.helper.ts
+++ src/Helpers/Systems/Profile/generateRandomAvatar.helper.ts
@@ -1,32 +1,42 @@
-import { AVATAR_OPTIONS } from '@/Consts/Avatars.const'
+import type { AvatarType } from '@/Types/Avatar.type'
 
-export const generateRandomAvatar = () => {
-  return {
-    clothes:
-      AVATAR_OPTIONS.clothes[
-        Math.floor(Math.random() * AVATAR_OPTIONS.clothes.length)
-      ],
+import {
+  EYE_OPTIONS,
+  FULL_CLOTHES_DEFINITIONS,
+  FULL_CLOTHES_MODELS,
+  HAIR_COLOR_OPTIONS,
+  HAIR_MODELS,
+  SKIN_OPTIONS
+} from '@/Consts/Avatars.const'
 
-    eyes: AVATAR_OPTIONS.eyes[
-      Math.floor(Math.random() * AVATAR_OPTIONS.eyes.length)
-    ],
+const getRandomOption = <Option>(options: readonly Option[]): Option =>
+  options[Math.floor(Math.random() * options.length)]
 
-    expression:
-      AVATAR_OPTIONS.expression[
-        Math.floor(Math.random() * AVATAR_OPTIONS.expression.length)
-      ],
-
-    hairColor:
-      AVATAR_OPTIONS.hairColor[
-        Math.floor(Math.random() * AVATAR_OPTIONS.hairColor.length)
-      ],
-
-    hair: AVATAR_OPTIONS.hair[
-      Math.floor(Math.random() * AVATAR_OPTIONS.hair.length)
-    ],
-
-    skin: AVATAR_OPTIONS.skin[
-      Math.floor(Math.random() * AVATAR_OPTIONS.skin.length)
+export const generateRandomAvatar = (): AvatarType => {
+  const fullClothesModel = getRandomOption(FULL_CLOTHES_MODELS)
+  const fullClothesDefinition =
+    FULL_CLOTHES_DEFINITIONS[
+      fullClothesModel as keyof typeof FULL_CLOTHES_DEFINITIONS
     ]
+  const fullClothesColor = getRandomOption(fullClothesDefinition.colors)
+
+  return {
+    expression: 'default',
+    skinColor: getRandomOption(SKIN_OPTIONS).color,
+    eyeColor: getRandomOption(EYE_OPTIONS).color,
+    hair: {
+      model: getRandomOption(HAIR_MODELS),
+      color: getRandomOption(HAIR_COLOR_OPTIONS).color
+    },
+    clothes: {
+      mode: 'fullClothes',
+      fullClothes: {
+        model: fullClothesModel,
+        color: fullClothesColor.color
+      },
+      top: null,
+      bottom: null
+    },
+    accessories: {}
   }
 }
